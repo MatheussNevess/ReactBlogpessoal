442adefcf201a4e1dd616baee6b65d98bd8cb5fc
Matheus_Neves <matheuswdn@gmail.com>
2024-08-02 16:18:07 -0300

    React do projeto Blog Pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+           <img src="https://news.microsoft.com/source/latam/wp-content/uploads/2023/09/Windows-11-1024x683.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
