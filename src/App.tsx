90ad340a2480d981ce790bb307ec5f873c6aaefc
Matheus_Neves <matheuswdn@gmail.com>
2024-08-07 16:41:41 -0300

    Adicionando Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Home from './paginas/home/Home';
 import './App.css';
-
-
+import Home from "../src/paginas/home/Home"
+import NavBar from './components/navBar/navBar';
+import Footer from './components/footer/footer';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Login from './paginas/login/Login';
 function App() {
   return (
-   <Home />
+    <>
+    <BrowserRouter>
+      <NavBar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+          </Routes>
+        </div>
+          <Footer />
+    </BrowserRouter>
+    </>
   );
 }
-
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,28 @@
-import './Home.css';
+import homeLogo from "../../assets/home.jpg"
+import "./Home.css"
 
-function Home(){
-    return (
-        <>
-            <h1 className="titulo">Home</h1>
-           <img src="https://news.microsoft.com/source/latam/wp-content/uploads/2023/09/Windows-11-1024x683.jpg" alt="Imagem Tela Inicial" className="img"/>
-        </>
-    );
+function home() {
+  return (
+    <>
+        <div className="bg-gray-900 text-white' : 'text-gray-300  hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium">
+            <div className="container grid grid-cols-2 text-white">
+                <div className="flex flex-col gap-4 items-center justify-center py-4">
+                    <h2 className="text-5xl font-bold">Seja bem vindo!</h2>
+                    <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
+
+                    <div className="flex justify-around gap-4">
+                        <button className="bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium">Ver postagens</button>
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <img src={homeLogo} alt="Logo Home" className="w-2/3" />
+                </div>
+            </div>
+        </div>
+    </>
+  )
 }
 
-export default Home;
+export default home
