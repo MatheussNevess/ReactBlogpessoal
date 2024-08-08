b427d8a75d14e5f00a90cac2995bf83feeec4a32
Matheus_Neves <matheuswdn@gmail.com>
2024-08-08 19:14:35 -0300

    Implementando função de Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
+import React from 'react';
 import './App.css';
-import Home from "../src/paginas/home/Home"
-import NavBar from './components/navBar/navBar';
+
+import Navbar from './components/navBar/navBar';
 import Footer from './components/footer/footer';
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Login from './paginas/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <NavBar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/home' element={<Home />}></Route>
-          </Routes>
-        </div>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
           <Footer />
-    </BrowserRouter>
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,24 +1,25 @@
-import {FacebookLogo, InstagramLogo, LinkedinLogo} from "@phosphor-icons/react"
+import React from 'react'
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
-    return (
-      <>
-          <div className="flex justify-center bg-gray-800 text-white">
-              <div className="container flex flex-col items-center py-4">
-                  <p className="text-xl font-bold">Blog pessoal Matheus Neves | Copyright</p>
-                  
-                  <p className="text-lg">Acesse nossas redes sociais</p>
-                  
-                  <div className="flex gap-2">
-                      <LinkedinLogo size={48} weight="bold" />
-                      <InstagramLogo size={48} weight="bold" />
-                      <FacebookLogo size={48} weight="bold" />
+ 
   
-                  </div>
-              </div>
+
+  return (
+    <>
+        <div className="flex justify-center bg-indigo-900 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
+            <p className='text-lg'>Acesse nossas redes sociais</p>
+            <div className='flex gap-2'>
+              <LinkedinLogo size={48} weight='bold' />
+              <InstagramLogo size={48} weight='bold' />
+              <FacebookLogo size={48} weight='bold' />
+            </div>
           </div>
+        </div>
       </>
-    )
-  }
-  
-  export default Footer
+  )
+}
+
+export default Footer
